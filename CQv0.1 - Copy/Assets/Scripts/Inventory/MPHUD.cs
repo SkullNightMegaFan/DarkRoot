2d6789008e62d6f0475d26a5d8f9cda1fc8942c0
BrentonFigures-Mormon <bfigures@gmu.edu>
2023-11-06 11:20:56 -0500

    minor update
    just basic changes to the ammo hud script so players know how many bullets they have.

--- CQv0.1 - Copy/Assets/Scripts/Inventory/MPHUD.cs
+++ CQv0.1 - Copy/Assets/Scripts/Inventory/MPHUD.cs
@@ -6,15 +6,18 @@ using UnityEngine.UI;
 public class MPHUD : MonoBehaviour {
 
     public PlayerStatus playerStatus;
+    public PlayerController playerController;
     Text hudText;
-
 	// Use this for initialization
 	void Start () {
         hudText = GetComponent<Text>();
+        //playerAmmo = playerController.currentAmmo;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-      //  hudText.text = "MP: " + playerStatus.GetCurrentMP() + "/" + playerStatus.MaxMP;
+        hudText.text = "Ammo: " + playerController.dodgeForce + "/" + playerStatus.playerHealth;// + "/" + playerStatus.MaxMP;
+        //hudText.text = "Ammo:" + playerController.currentAmmo.ToFloat();
+
     }
 }
