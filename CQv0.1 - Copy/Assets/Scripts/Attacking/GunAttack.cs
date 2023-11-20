3779061968a6b00d711cd42cee0735958caaca9e
BrentonFigures-Mormon <bfigures@gmu.edu>
2023-11-20 02:03:20 -0500

    NPC Update
    Players can now talk to NPC's with more than one line of dialogue. In addition, the player is completely invincible and can no longer move, shoot, burrow, reload, or dodge while talking to an NPC. Another assest added to the game.

--- CQv0.1 - Copy/Assets/Scripts/Attacking/GunAttack.cs
+++ CQv0.1 - Copy/Assets/Scripts/Attacking/GunAttack.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GunAttack : MonoBehaviour
 {
@@ -10,23 +11,31 @@ public class GunAttack : MonoBehaviour
     protected internal Vector3 bulletSpawnLocation;
     protected internal Vector2 direction;
 
+    private InputAction fire;
+
     void Update()
     {
         bulletSpawnLocation = bulletOriginPoint.transform.position;
         FindTarget();
     }
 
+//finds the cursor and then puts the direction of the cursor
     virtual protected void FindTarget()
     {
         Vector3 mousePosition =  Camera.main.GetComponent<MouseBehaviour>().mousePosition;
         direction = (mousePosition - bulletSpawnLocation).normalized;
     }
 
+//this is what spawns the bullet
     public void Attack()
     {
         SpawnBullet();
     }
 
+//personally with this, I think I should put ammo and reload time of the gun here. 
+//or at least within this script and have the player controller read the variables from the script. 
+//Alas, time is an ever marching dictator. I'll leave the comment here so future
+// team members or just me can get to work on it
     void SpawnBullet()
     {// Instantiate at position (x) and zero rotation.
         GameObject bullet = Instantiate(bulletType, bulletSpawnLocation, Quaternion.identity);
