7785094c26c0c60a1286fa8cb494fb1b96ec7bb9
Joy Shideler <wink2game@gmail.com>
2023-11-24 18:23:13 -0500

    adding art stuff

---
+++ CQv0.1 - Copy/Assets/RotateToPlayer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateToPlayer : MonoBehaviour
+{
+    protected internal GameObject player;
+    public GameObject rotationAnchor;
+    Vector2 direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 rotationAnchorPosition = transform.position;
+        Vector3 playerPosition = player.transform.position;
+        LookToPlayer(playerPosition, rotationAnchorPosition);
+    }
+    void LookToPlayer(Vector3 target, Vector3 anchor)
+    {
+        direction = target - anchor;
+
+        float angle = Vector2.SignedAngle(Vector2.down, direction);
+        transform.eulerAngles = new Vector3(0, 0, angle);
+    }
+}
